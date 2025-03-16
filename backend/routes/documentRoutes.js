f5489bb26768689c4e33c0a598d6fa022dc3f402
prachi-stack <deopaprachi60@gmail.com>
2025-03-16 23:28:30 +0530

    Update documentRoutes.js

--- backend/routes/documentRoutes.js
+++ backend/routes/documentRoutes.js
@@ -8,16 +8,12 @@ import {
 
 const router = express.Router();
 
-// Route to create a new document
 router.post("/documents", createDocument);
 
-// Route to fetch all documents with pagination
 router.get("/documents", getAllDocuments);
 
-// Route to fetch a single document by ID
 router.get("/documents/:id", getDocumentById);
 
-// Route to update a document
 router.put("/documents/:id", updateDocument);
 
 export default router;
